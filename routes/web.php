ed2bdfce5270cb474f3229d4d492a5450db5fd9e
hackshade-techs <hackshadetechs@gmail.com>
2018-02-16 13:35:40 +0300

    Added routes for pages generated by pagemanger Add function for the pagemanager in pagecontroller

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::group([
     CRUD::resource('gallery', 'GalleryCrudController');
     CRUD::resource('contact', 'ContactCrudController');
 });
+
+/** CATCH-ALL ROUTE for Backpack/PageManager - needs to be at the end of your routes.php file  **/
+Route::get('{page}/{subs?}', ['uses' => 'PageController@index'])
+    ->where(['page' => '^((?!admin).)*$', 'subs' => '.*']);
